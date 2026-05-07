cffb7393982042aab0107c078fcfba7c0b2a7f8c
Mohammed Farjan <your-email@example.com>
2026-05-07 10:38:39 +0530

    Lab Practical

---
+++ reactapp/src/components/FormHandling.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FormHandling() {
+  return (
+    <div>
+        <input type="text">User Name : </input>
+        <input type="button" className='btn'>Click</input>
+    </div>
+
+  )
+}
+
+export default FormHandling
